6bd564603b389b266f47a77c939915be3675cbbf
TullioTambone <tulliotambone97@gmail.com>
2023-04-14 16:01:51 +0200

    fine secondo snack

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,4 +54,47 @@ biciclette.forEach((element) => {
 const {nome, peso} = myPeso;
 document.getElementById('snack-1').innerHTML = `
     <h2>la bici di tipo ${nome} pesa ${peso}kg ed è la più leggera</h2>
-    `
+`
+
+//------------------------------------------ SNACK 2 -------------------------------------------------------
+/*
+Snack2
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà:
+Punti fatti e falli subiti.
+Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+*/
+
+
+
+const squadre = [
+    {
+        name: 'napoli',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        name: 'roma',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        name: 'fiorentina',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        name: 'milan',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    }
+]
+
+squadre.forEach((element) => {
+    element.puntiFatti = Math.floor(Math.random() * 10 ) + 1;
+    element.falliSubiti = Math.floor(Math.random() * 10 ) +1;
+});
+
+const nomeFalli = squadre.map(({ name, falliSubiti }) => ({ name, falliSubiti }));
+console.log(nomeFalli);
