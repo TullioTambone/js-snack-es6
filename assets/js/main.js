51d3b17902f646898f9aad13231b2ca023b1e4f5
TullioTambone <tulliotambone97@gmail.com>
2023-04-14 14:31:29 +0200

    inizio snack 1 + creazione array di oggetti biciclette

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/*
+Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+*/
+
+const biciclette = [
+    {
+        nome : 'mountain bike',
+        peso : 10,
+    },
+    {
+        nome : 'bmx',
+        peso : 15,
+    },
+    {
+        nome : 'graziella',
+        peso : 20,
+    },
+    {
+        nome : 'tandem',
+        peso : 30,
+    }
+]
